23c7419dd345ae985eba54ec16409d683f05dbbc
akrisfx <vpotach@gmail.com>
2022-10-19 15:43:15 +0300

    recomposed code - add struct for work in func thread - small optimised on constexpr

--- Source.cpp
+++ Source.cpp
@@ -19,13 +19,15 @@ using std::cin;
 using std::endl;
 
 constexpr double pi = std::numbers::pi;
-constexpr double l = 1.5;
+constexpr double l = 1.0;
 constexpr double h = 0.125;
 constexpr double n_l = l / h;
 constexpr int n_l_int = l / h;
 
 constexpr double tau = 0.0001;
 constexpr int n_tau = 30;
+
+constexpr int val_on_core = n_l_int / 4;
 
 
 //void tau_list(double*& arr, const int& size_arr, const double& step) {
@@ -43,19 +45,58 @@ constexpr double* tau_list(const double& step)
 	}
 	return res;
 }
-//struct arrs
+struct arrs {
+	double prev_str[n_l_int + 1]{ 0 };
+	double current_str[n_l_int + 1]{ 0 };
+	const double* arr_tau = tau_list<n_tau>(tau);
+	const double* arr_l = tau_list<n_l_int>(h);
+};
 
-const double* arr_tau = tau_list<n_tau>(tau);
-const double* arr_l = tau_list<n_l_int>(h);
+
 
 
 double fi(const double& x) {
 	return 4 * (sin(2 * pi * x) * pow(cos(pi * x), 2));
 }
 
-//void in_Thread(auto& n_l_int, int* arr_l[], double &t, const double &tau, double* prev_str[], double* currrent_str, int) {
-//
-//}
+void in_Thread(int start, int end, arrs& a, double& t) {
+		for (int j = start; j < end; j++) {
+			double x = a.arr_l[j];
+			double u_x_t;
+			//cout << std::setw(8) << x;
+			if (t == 0) {
+				if (x == 0 || x == l) {
+					u_x_t = 0;
+				}
+				else {
+					u_x_t = fi(x);
+					u_x_t = (round(u_x_t * 1000) / 1000);
+					if (u_x_t == 0.0 || u_x_t == -0.0) {
+						u_x_t = 0;
+					}
+				}
+				//m.lock();
+				a.prev_str[j] = u_x_t;
+				//m.unlock();
+			}
+			else {
+				if (x == 0 || x == l) {
+					u_x_t = 0;
+					//m.lock();
+					a.current_str[j] = u_x_t;
+					//m.unlock();
+				}
+				else {
+					//m.lock();
+					u_x_t = tau * ((a.prev_str[j - 1] - 2 * a.prev_str[j] + a.prev_str[j + 1]) / pow(h, 2)) + a.prev_str[j];
+					a.current_str[j] = u_x_t;
+					//m.unlock();
+				}
+			}
+			//u_x_t = (round(u_x_t * 1000) / 1000);
+			//cout << std::setw(8) << u_x_t;
+		}
+}
 
 //void copy_arr(double[] &qwdn, double[n_l_int] &copy) {
 //
@@ -70,10 +111,10 @@ int main() {
 		return 404;
 	}
 	
-	int val_on_core = n_l_int / 4;
+	 
+	//cout << val_on_core << endl;
 	
 	std::mutex m;
-	cout << val_on_core << endl;
 
 	int mlt_for_loop[6];
 	for (int i = 0; i < 4; i++) {
@@ -82,198 +123,45 @@ int main() {
 	}
 
 
-	/*tau_list(arr_tau, n_tau, tau);
-
-	
-	tau_list(arr_l, n_l_int, h);*/
-
-	double prev_str[n_l_int + 1] {0};
-	double current_str[n_l_int + 1] {0};
-
-	//for (int i = 0; i < n_tau + 1; i++) {
-	//	//arr_tau[i] = tau * i;
-	//	cout << arr_tau[i] << ' ';
-	//}
-	//cout << endl << endl;
-	//for (int i = 0; i < n_l_int + 1; i++) {
-	//	cout << arr_l[i] << ' ';
-	//}
+	arrs a;
 
 
 	cout << endl << std::setw(10) << "        |";
 
-	for (int i = 0; i < n_l_int + 1; i++) {
-		cout << std::setw(8) << arr_l[i];
+	for (int i = 0; i <= n_l_int; i++) {
+		cout << std::setw(8) << a.arr_l[i];
 	}
 	cout << endl;
 
-	for (int i = 0; i < n_l_int; i++) {
+	for (int i = 0; i <= n_l_int; i++) {
 		cout << "_________";
 	}
 
 	cout << endl;
 	cout << '\n';
+	
 
-	for (int i = 0; i < n_tau + 1; i++) {
-		double t = arr_tau[i];
+
+	for (int i = 0; i <= n_tau; i++) {
+		double t = a.arr_tau[i];
 		cout << std::setw(6) << t <<  "   |";
 		int thread_test = 0;
 
 		std::thread t1([&]() {
-			for (int j = 0; j < mlt_for_loop[1]; j++) {
-				double x = arr_l[j];
-				double u_x_t;
-				//cout << std::setw(8) << x;
-				if (t == 0) {
-					if (x == 0 || x == l){
-						u_x_t = 0;
-					}
-					else {
-						u_x_t = fi(x);
-						u_x_t = (round(u_x_t * 1000) / 1000);
-						if (u_x_t == 0.0 || u_x_t == -0.0) {
-							u_x_t = 0;
-						}
-					}
-					//m.lock();
-					prev_str[j] = u_x_t;
-					//m.unlock();
-				}
-				else {
-					if (x == 0 || x == l) {
-						u_x_t = 0;
-						//m.lock();
-						current_str[j] = u_x_t;
-						//m.unlock();
-					}
-					else {
-						//m.lock();
-						u_x_t = tau * ((prev_str[j - 1] - 2 * prev_str[j] + prev_str[j + 1]) / pow(h, 2)) + prev_str[j];
-						current_str[j] = u_x_t;
-						//m.unlock();
-					}
-				}
-				//u_x_t = (round(u_x_t * 1000) / 1000);
-				//cout << std::setw(8) << u_x_t;
-			}
+			in_Thread(mlt_for_loop[0], mlt_for_loop[1], a, t);
 			});
 
 		std::thread t2([&]() {
-			for (int j = mlt_for_loop[1]; j < mlt_for_loop[2]; j++) {
-				double x = arr_l[j];
-				double u_x_t;
-				//cout << std::setw(8) << x;
-				if (t == 0) {
-					if (x == 0 || x == l) {
-						u_x_t = 0;
-					}
-					else {
-						u_x_t = fi(x);
-						u_x_t = (round(u_x_t * 1000) / 1000);
-						if (u_x_t == 0.0 || u_x_t == -0.0) {
-							u_x_t = 0;
-						}
-					}
-					//m.lock();
-					prev_str[j] = u_x_t;
-					//m.unlock();
-				}
-				else {
-					if (x == 0 || x == l) {
-						u_x_t = 0;
-						//m.lock();
-						current_str[j] = u_x_t;
-						//m.unlock();
-					}
-					else {
-						//m.lock();
-						u_x_t = tau * ((prev_str[j - 1] - 2 * prev_str[j] + prev_str[j + 1]) / pow(h, 2)) + prev_str[j];
-						current_str[j] = u_x_t;
-						//m.unlock();
-					}
-				}
-				//u_x_t = (round(u_x_t * 1000) / 1000);
-				//cout << std::setw(8) << u_x_t;
-			}
-			});
+			in_Thread(mlt_for_loop[1], mlt_for_loop[2], a, t);
+		});
 
 		std::thread t3([&]() {
-			for (int j = mlt_for_loop[2]; j < mlt_for_loop[3]; j++) {
-				double x = arr_l[j];
-				double u_x_t;
-				//cout << std::setw(8) << x;
-				if (t == 0) {
-					if (x == 0 || x == l) {
-						u_x_t = 0;
-					}
-					else {
-						u_x_t = fi(x);
-						u_x_t = (round(u_x_t * 1000) / 1000);
-						if (u_x_t == 0.0 || u_x_t == -0.0) {
-							u_x_t = 0;
-						}
-					}
-					//m.lock();
-					prev_str[j] = u_x_t;
-					//m.unlock();
-				}
-				else {
-					if (x == 0 || x == l) {
-						u_x_t = 0;
-						//m.lock();
-						current_str[j] = u_x_t;
-						//m.unlock();
-					}
-					else {
-						//m.lock();
-						u_x_t = tau * ((prev_str[j - 1] - 2 * prev_str[j] + prev_str[j + 1]) / pow(h, 2)) + prev_str[j];
-						current_str[j] = u_x_t;
-						//m.unlock();
-					}
-				}
-				//u_x_t = (round(u_x_t * 1000) / 1000);
-				//cout << std::setw(8) << u_x_t;
-			}
-			});
+			in_Thread(mlt_for_loop[2], mlt_for_loop[3], a, t);
+		});
 
 		std::thread t4([&]() {
-			for (int j = mlt_for_loop[3]; j < n_l_int + 1; j++) {
-				double x = arr_l[j];
-				double u_x_t;
-				//cout << std::setw(8) << x;
-				if (t == 0) {
-					if (x == 0 || x == l) {
-						u_x_t = 0;
-					}
-					else {
-						u_x_t = fi(x);
-						u_x_t = (round(u_x_t * 1000) / 1000);
-						if (u_x_t == 0.0 || u_x_t == -0.0) {
-							u_x_t = 0;
-						}
-					}
-					//m.lock();
-					prev_str[j] = u_x_t;
-					//m.unlock();
-				}
-				else {
-					if (x == 0 || x == l) {
-						u_x_t = 0;
-						//m.lock();
-						current_str[j] = u_x_t;
-						//m.unlock();
-					}
-					else {
-						//m.lock();
-						u_x_t = tau * ((prev_str[j - 1] - 2 * prev_str[j] + prev_str[j + 1]) / pow(h, 2)) + prev_str[j];
-						current_str[j] = u_x_t;
-						//m.unlock();
-					}
-				}
-				//u_x_t = (round(u_x_t * 1000) / 1000);
-				//cout << std::setw(8) << u_x_t;
-			}
-			});
+			in_Thread(mlt_for_loop[3], n_l_int, a, t);
+		});
 
 		t1.join();
 		t2.join();
@@ -281,13 +169,13 @@ int main() {
 		t4.join();
 
 		for (int j = 0; j < n_l_int + 1; j++) {
-			double u_x_t = (round(current_str[j] * 1000) / 1000);
+			double u_x_t = (round(a.current_str[j] * 1000) / 1000);
 			cout << std::setw(8) << u_x_t;
 		}
 
 		if (i != 0) {
 			for (int k = 0; k < n_l_int; k++) {
-				prev_str[k] = current_str[k];
+				a.prev_str[k] = a.current_str[k];
 			}
 		}
 		
@@ -295,8 +183,8 @@ int main() {
 		cout << endl;
 		//cout << thread_test << endl;
 	}
-	delete[] arr_tau;
-	delete[] arr_l;
+	/*delete[] arr_tau;
+	delete[] arr_l;*/
 
 	//cout << pi << endl;
 	system("pause");
